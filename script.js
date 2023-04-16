2a50b212cb82e1040c289cdd7fd1025e70a2b6ec
paolanocom <paolanocom@gmail.com>
2023-04-16 16:40:10 +0800

    Fixed a very silly error :D

--- script.js
+++ script.js
@@ -1,45 +1,120 @@
-<html>
-  <head>
-    <title>ChatDapp</title>
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    />
-    <script
-      src="https://cdn.ethers.io/lib/ethers-5.2.umd.min.js"
-      type="application/javascript"
-    ></script>
-    <script src="script.js"></script>
-  </head>
-  <body onload="connectMetamask()" class="p-5">
-    <div class="container mt-5">
-      <button
-        type="button"
-        class="btn btn-warning"
-        onclick="connectMetamask()"
-        id="connectMetamask"
-      >
-        Connect MetaMask
-      </button>
-      <button type="button" class="btn btn-dark" onclick="getMessage()">
-        Load Last Message
-      </button>
-      <div class="container px-0" id="messages"></div>
-    </div>
-    <div class="container mt-5">
-      <form>
-        <div class="form-group">
-          <label for="recipientAddress">Send To:</label>
-          <input type="text" class="form-control" id="recipientAddress" />
-        </div>
-        <div class="form-group">
-          <label for="message">Message</label>
-          <textarea class="form-control" id="message"></textarea>
-        </div>
-        <button type="button" class="btn btn-primary" onclick="sendMessage()">
-          Send Message
-        </button>
-      </form>
-    </div>
-  </body>
-</html>
+const contractAddress = "0x9A3209d94B909682BB1F936c5f7CC334Edf76077";
+const contractABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "recipient",
+        type: "address"
+      },
+      {
+        internalType: "string",
+        name: "message",
+        type: "string"
+      }
+    ],
+    name: "sendMessage",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "getMessage",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "sender",
+        type: "address"
+      }
+    ],
+    name: "getMessageFrom",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  }
+];
+
+let signer;
+// const signer = provider.getSigner();
+let contract;
+
+async function connectMetamask() {
+  // console.log("Attempting to connect Metamask");
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  await provider.send("eth_requestAccounts", []);
+  // console.log("Awaiting provider");
+  signer = await provider.getSigner();
+  contract = new ethers.Contract(contractAddress, contractABI, signer);
+
+  if (signer) {
+    let connectBtn = document.getElementById("connectMetamask");
+    let address = await signer.getAddress();
+    let truncAddress = address.slice(0, 5) + "..." + address.slice(-4);
+    connectBtn.innerHTML = truncAddress;
+    connectBtn.classList.remove("btn-warning");
+    connectBtn.classList.add("btn-dark");
+  }
+
+  // Load last msg immediately after connecting metamask
+  await getMessage();
+}
+
+async function sendMessage() {
+  const recipientAddress = document.getElementById("recipientAddress").value;
+  const message = document.getElementById("message").value;
+
+  // send the transaction and wait for confirmation
+  const tx = await contract.sendMessage(recipientAddress, message);
+  await tx.wait();
+
+  // load the last message that was sent
+  await getMessage();
+}
+
+let lastLoadedMessage = "";
+
+async function getMessage() {
+  let newMessage = await contract.getMessage();
+  let messagesDiv = document.getElementById("messages");
+
+  // compare the new message to the last loaded message
+  if (newMessage !== lastLoadedMessage) {
+    let messageNode = document.createElement("p");
+    messageNode.classList.add(
+      "lead",
+      "bg-primary",
+      "text-light",
+      "py-2",
+      "px-3",
+      "my-3",
+      "mx-0",
+      "rounded-pill"
+    );
+    messageNode.innerText = newMessage;
+    messagesDiv.appendChild(messageNode);
+
+    // update the last loaded message
+    lastLoadedMessage = newMessage;
+    console.log(`Last message received: ${newMessage}`);
+  } else {
+    alert("Last message already loaded.");
+  }
+}
